5a56894f11dddf4210ae8f626595242486c11882
Kristoffer Robin Canlas <kristofferrobincanlas@gmail.com>
2020-05-17 18:53:33 +0800

    added the quotes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -31,6 +31,9 @@ export class App extends React.Component<App.Props, App.State> {
 	}
 
 	public render() {
+		const quote: string = `"I've missed more than 9000 shots in my career. I've lost almost 300 games. 26 times, I've been trusted to take the game winning shot and missed. I've failed over and over and over again in my life. And that is why I succeed."`;
+		const author: string = '-Michael Jordan';
+
 		return (			
 			<React.Fragment>
 				{this.state.isLoading ?
@@ -39,6 +42,10 @@ export class App extends React.Component<App.Props, App.State> {
 						<Header />
 						<div className={style.jumpman}>
 							<Jumpman />
+							<div>
+								<p>{quote}</p>
+								<p>{author}</p>
+							</div>
 						</div>
 						<HomePage />
 						<ShopPage />
--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import * as style from './style.css';
 import { Image } from 'semantic-ui-react';
-import wallpaper from './wallpaper.jpg';
 
 export namespace HomePage {
 	export interface Props {
@@ -14,7 +13,7 @@ export const HomePage: React.FC<HomePage.Props> = (props: HomePage.Props) => {
 	return (
 		<div id='home' className={style.home}>
 			<div>
-				<Image src={wallpaper} />
+				<Image src='https://res.cloudinary.com/speedforce/image/upload/v1589709329/jordan/wallpaper_ivene5.jpg' />
 			</div>
 		</div>
 	);
